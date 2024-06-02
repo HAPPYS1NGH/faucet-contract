78dcf34e257effcc34e1835a8db016b78b054f42
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-02 18:35:45 +0530

    feat: added more test

--- test/Faucet.t.sol
+++ test/Faucet.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/Faucet.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FaucetTest is Test {
     Faucet public faucet;
@@ -102,6 +103,81 @@ contract FaucetTest is Test {
         emit TokensDripped(user, fid);
 
         // Drip tokens to user
+        faucet.dripTokensToAddress(user, fid, amount);
+
+        vm.stopPrank();
+    }
+
+    function test_DripTokensToMultipleUsers() public {
+        vm.startPrank(owner);
+
+        uint256 amount = 0.01 ether;
+        uint256 fid1 = 123;
+        uint256 fid2 = 456;
+        address user2 = address(3);
+
+        // Give the user2 some initial balance
+        vm.deal(user2, 0.05 ether);
+
+        // Drip tokens to user1
+        faucet.dripTokensToAddress(user, fid1, amount);
+        // Drip tokens to user2
+        faucet.dripTokensToAddress(user2, fid2, amount);
+
+        assertEq(user.balance, 0.06 ether, "User1 balance should increase by amount dripped");
+        assertEq(user2.balance, 0.06 ether, "User2 balance should increase by amount dripped");
+
+        vm.stopPrank();
+    }
+
+    function test_VerifyContractBalanceAfterMultipleDrips() public {
+        vm.startPrank(owner);
+
+        uint256 amount = 0.01 ether;
+        uint256 fid1 = 123;
+        uint256 fid2 = 456;
+        address user2 = address(3);
+
+        // Give the user2 some initial balance
+        vm.deal(user2, 0.05 ether);
+
+        // Drip tokens to user1
+        faucet.dripTokensToAddress(user, fid1, amount);
+        // Drip tokens to user2
+        faucet.dripTokensToAddress(user2, fid2, amount);
+
+        uint256 expectedBalance = 1 ether - 2 * amount;
+        assertEq(
+            address(faucet).balance, expectedBalance, "Faucet contract balance should decrease by the dripped amounts"
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_ReceiveFunction() public {
+        vm.deal(user, 1 ether);
+        vm.prank(user);
+        (bool success,) = address(faucet).call{value: 0.1 ether}("");
+        assertTrue(success, "Receive function should accept ether");
+        assertEq(address(faucet).balance, 1.1 ether, "Faucet balance should increase by received amount");
+    }
+
+    function test_FallbackFunction() public {
+        vm.deal(user, 1 ether);
+        vm.prank(user);
+        (bool success,) = address(faucet).call{value: 0.1 ether}("random_data");
+        assertTrue(success, "Fallback function should accept ether");
+        assertEq(address(faucet).balance, 1.1 ether, "Faucet balance should increase by received amount");
+    }
+
+    function test_OnlyOwnerCanDripTokens() public {
+        vm.startPrank(user); // Non-owner trying to drip tokens
+
+        uint256 amount = 0.01 ether;
+        uint256 fid = 123;
+
+        // Try to drip tokens as a non-owner
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user));
         faucet.dripTokensToAddress(user, fid, amount);
 
         vm.stopPrank();
